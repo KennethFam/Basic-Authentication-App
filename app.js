1c45cbb52b398f0e45280faa04401b45c1e778d4
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-05-21 18:05:21 -0700

    added commits to session options

--- app.js
+++ app.js
@@ -54,12 +54,12 @@ app.set("view engine", "ejs");
 
 app.use(session({ 
   store: new (require(`connect-pg-simple`)(session))({
-    conString: process.env.DATABASE_URL,
-    createTableIfMissing: true
+    conString: process.env.DATABASE_URL, // connection string for the db
+    createTableIfMissing: true // create session db if it does not exist
   }),
-  secret: process.env.SESSION_SECRET, 
-  resave: false, 
-  saveUninitialized: false,
+  secret: process.env.SESSION_SECRET, // set secret
+  resave: false, // session is only saved if it was modified, affects existing sessions
+  saveUninitialized: false, // only save session if you modify it, affects new sessions
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
 }));
 app.use(passport.session());
